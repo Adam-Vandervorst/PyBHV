f83f31836f2a6e4eede068199dda5d5e4f781323
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-03-28 13:14:24 +0200

    Expose NumPy implementation in init

--- bhv/__init__.py
+++ bhv/__init__.py
@@ -1,4 +1,5 @@
 from .abstract import AbstractBHV
 from .shared import DIMENSION
 from .pytorch import TorchBoolBHV, TorchPackedBHV
+from .np import NumPyBoolBHV, NumPyPacked8BHV, NumPyPacked64BHV
 
