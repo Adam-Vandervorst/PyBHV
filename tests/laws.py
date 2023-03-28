10235f08aacfd3fa5a427f4b4f2c573ff7a3f36f
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-03-28 21:37:08 +0200

    Update testing mechanism

--- tests/laws.py
+++ tests/laws.py
@@ -1,182 +1,79 @@
-import unittest
 from time import monotonic
-from itertools import product
+from itertools import product, groupby
 
 from bhv.abstract import AbstractBHV
 from bhv.np import NumPyBoolBHV, NumPyPacked8BHV, NumPyPacked64BHV
 from bhv.pytorch import TorchBoolBHV, TorchPackedBHV
 
 
-ALL = [NumPyBoolBHV, NumPyPacked8BHV, NumPyPacked64BHV, TorchBoolBHV, TorchPackedBHV]
+def associative(m): return lambda x, y, z: m(m(x, y), z) == m(x, m(y, z))
+def commutative(m): return lambda x, y: m(x, y) == m(y, x)
+def idempotent(m): return lambda x: m(x, x) == x
+def self_inverse(m, u): return lambda x: m(x, x) == u
+def self_inverse_op(f): return lambda x: f(f(x)) == x
+def equals(f, a, b): return lambda: f(a) == b
+def right_unit(m, u): return lambda x: m(x, u) == x
+def right_absorbing(m, z): return lambda x: m(x, z) == z
+def absorptive(m, w): return lambda x, y: m(x, w(x, y)) == x
+def distributive(m, w): return lambda x, y, z: m(x, w(y, z)) == w(m(x, y), m(x, z))
+def demorgan(f, m, w): return lambda x, y: f(m(x, y)) == w(f(x), f(y))
+def drop_left(f, m): return lambda x, y: f(m(x, y)) == m(f(x), y)
+def expand_single_inner(f, k, m, w): return lambda x, y: True  # k(x, y) == m(w(x, f(y)), w(f(x), y))
+def expand_single_outer(f, k, m, w): return lambda x, y: True  # k(x, y) == m(w(x, y), f(m(x, y)))
 
 
-def for_implementations(impls):
-    def wrap(law):
-        for impl in impls:
-            name = f"{law.__name__}_{impl.__name__}"
-            globals()[name] = type(name, (TestImpl, law, unittest.TestCase), dict(impl=impl))
-    return wrap
+def lattice(join): return [associative(join), commutative(join), idempotent(join)]
+def bounded_lattice(join, top, bot): return lattice(join) + [right_unit(join, bot), right_absorbing(join, top)]
+def xor_props(xor_, bot): return [associative(xor_), commutative(xor_), self_inverse(xor_, bot), right_unit(xor_, bot)]
+def not_props(not_, bot, top): return [self_inverse_op(not_), equals(not_, top, bot), equals(not_, bot, top)]
+def or_and_props(or_, and_): return [distributive(or_, and_), distributive(and_, or_), absorptive(or_, and_), absorptive(and_, or_)]
 
 
-class TestImpl:
-    impl: AbstractBHV
+def bhv_props(impl: AbstractBHV):
+    extra = [
+        demorgan(impl.__invert__, impl.__or__, impl.__and__),
+        demorgan(impl.__invert__, impl.__and__, impl.__or__),
+        drop_left(impl.__invert__, impl.__xor__),
+        expand_single_inner(impl.__invert__, impl.__xor__, impl.__and__, impl.__or__),
+        expand_single_outer(impl.__invert__, impl.__xor__, impl.__or__, impl.__and__),
+        distributive(impl.__and__, impl.__xor__)
+    ]
 
-    @classmethod
-    def setUpClass(cls):
-        print(f"Testing {cls.__name__}...")
-        cls._t0 = monotonic()
-
-        cls.extrema = [cls.impl.ZERO, cls.impl.ONE]
-        cls.shared = cls.extrema + [cls.impl.rand() for _ in range(3)]
-        cls.xs = cls.shared + [cls.impl.rand() for _ in range(5)]
-        cls.ys = cls.shared + [cls.impl.rand() for _ in range(2)]
-        cls.zs = cls.shared + [cls.impl.rand() for _ in range(2)]
-
-    @classmethod
-    def tearDownClass(cls):
-        t = monotonic() - cls._t0
-        print(f"Done testing {cls.__name__} ({t*1000:.3} ms)")
-
-
-class Comm:
-    def test_associative(self):
-        for x, y, z in product(self.xs, self.ys, self.zs):
-            self.assertEqual(self.op(self.op(x, y), z), self.op(x, self.op(y, z)))
+    return (
+        bounded_lattice(impl.__and__, impl.ZERO, impl.ONE) +
+        bounded_lattice(impl.__or__, impl.ONE, impl.ZERO) +
+        xor_props(impl.__xor__, impl.ZERO) +
+        not_props(impl.__invert__, impl.ZERO, impl.ONE) +
+        or_and_props(impl.__or__, impl.__and__) +
+        extra)
 
 
-class Assoc:
-    def test_commutative(self):
-        for x, y in product(self.xs, self.ys):
-            self.assertEqual(self.op(x, y), self.op(y, x))
+def run_for(impl: AbstractBHV, ts):
+    extrema = [impl.ZERO, impl.ONE]
+    shared = extrema + [impl.rand() for _ in range(3)]
+
+    argts = {k: list(vs) for k, vs in groupby(ts, lambda f: f.__code__.co_argcount)}
+
+    def rec(args, depth):
+        if depth in argts:
+            for tn in argts[depth]:
+                assert tn(*args), f"property {tn.__qualname__} failed on {args} using implementation {impl.__name__}"
+            for x in shared:
+                rec(args + [x], depth + 1)
+
+    rec([], 0)
 
 
-class JoinLattice(Comm, Assoc):
-    def test_idempotent(self):
-        for x in self.xs:
-            self.assertEqual(self.op(x, x), x)
+def run():
+    all_implementations = [NumPyBoolBHV, NumPyPacked8BHV, NumPyPacked64BHV, TorchBoolBHV, TorchPackedBHV]
 
-
-class SelfInv:
-    def test_self_inverse(self):
-        for x in self.xs:
-            self.assertEqual(self.op(x, x), self.bot)
-
-
-class SelfInvOp:
-    def test_self_inverse_op(self):
-        for x in self.xs:
-            self.assertEqual(self.op(self.op(x)), x)
-
-
-class Inverse:
-    def test_extrema(self):
-        self.assertEqual(self.op(self.top), self.bot)
-        self.assertEqual(self.op(self.bot), self.top)
-
-
-class BottomIdentity:
-    def test_bottom_identity(self):
-        for x in self.xs:
-            self.assertEqual(self.op(x, self.bot), x)
-
-
-class BoundedJoinLattice(JoinLattice, BottomIdentity):
-    def test_top_absorbing(self):
-        for x in self.xs:
-            self.assertEqual(self.op(x, self.top), self.top)
-
-
-class Absorptive:
-    def test_absorptive(self):
-        for x, y in product(self.xs, self.ys):
-            self.assertEqual(self.op1(x, self.op2(x, y)), x)
-            self.assertEqual(self.op2(x, self.op1(x, y)), x)
-
-
-class Distributive:
-    def test_distributive(self):
-        for x, y, z in product(self.xs, self.ys, self.zs):
-            self.assertEqual(self.op1(x, self.op2(y, z)), self.op2(self.op1(x, y), self.op1(x, z)))
-            self.assertEqual(self.op2(x, self.op1(y, z)), self.op1(self.op2(x, y), self.op2(x, z)))
-
-
-class DeMorgan:
-    def test_demorgan(self):
-        for x, y in product(self.xs, self.ys):
-            self.assertEqual(self.op(self.op1(x, y)), self.op2(self.op(x), self.op(y)))
-            self.assertEqual(self.op(self.op2(x, y)), self.op1(self.op(x), self.op(y)))
-
-
-class DropSingle:
-    def test_drop_single(self):
-        for x, y in product(self.xs, self.ys):
-            self.assertEqual(self.op(self.op1(x, y)), self.op1(self.op(x), y))
-            self.assertEqual(self.op(self.op1(x, y)), self.op1(x, self.op(y)))
-
-
-class ExpandSingle:
-    def test_expand_single_inner(self):
-        for x, y in product(self.xs, self.ys):
-            self.assertEqual(self.op3(x, y), self.op1(self.op2(x, self.op(y)), self.op2(self.op(x), y)))
-
-    def test_expand_single_outer(self):
-        for x, y in product(self.xs, self.ys):
-            self.assertEqual(self.op3(x, y), self.op2(self.op1(x, y), self.op(self.op2(x, y))))
-
-
-@for_implementations(ALL)
-class TestAND(BoundedJoinLattice):
-    op = lambda _, x, y: x & y
-    top = property(lambda self: self.impl.ZERO)
-    bot = property(lambda self: self.impl.ONE)
-
-
-@for_implementations(ALL)
-class TestOR(BoundedJoinLattice):
-    op = lambda _, x, y: x | y
-    top = property(lambda self: self.impl.ONE)
-    bot = property(lambda self: self.impl.ZERO)
-
-
-@for_implementations(ALL)
-class TestXOR(Comm, Assoc, SelfInv, BottomIdentity):
-    op = lambda _, x, y: x ^ y
-    bot = property(lambda self: self.impl.ZERO)
-
-
-@for_implementations(ALL)
-class TestNOT(SelfInvOp, Inverse):
-    op = lambda _, x: ~x
-    bot = property(lambda self: self.impl.ZERO)
-    top = property(lambda self: self.impl.ONE)
-
-
-@for_implementations(ALL)
-class TestORAND(Absorptive, Distributive):
-    op1 = lambda _, x, y: x | y
-    op2 = lambda _, x, y: x & y
-
-
-@for_implementations(ALL)
-class TestORANDNOT(DeMorgan):
-    op = lambda _, x: ~x
-    op1 = lambda _, x, y: x | y
-    op2 = lambda _, x, y: x & y
-
-
-@for_implementations(ALL)
-class TestXORNOT(DropSingle):
-    op = lambda _, x: ~x
-    op1 = lambda _, x, y: x ^ y
-
-
-@for_implementations(ALL)
-class TestXORORANDNOT(ExpandSingle):
-    op = lambda _, x: ~x
-    op1 = lambda _, x, y: x | y
-    op2 = lambda _, x, y: x & y
-    op3 = lambda _, x, y: x ^ y
+    for impl in all_implementations:
+        print(f"Testing {impl.__name__}...")
+        t0 = monotonic()
+        run_for(impl, bhv_props(impl))
+        t = monotonic() - t0
+        print(f"took ({t*1000:.3} ms)")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    run()
