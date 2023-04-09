778845cafac1b1c1895467399adf543d42762970
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-04-09 13:49:38 +0200

    Add minimal winnow and majority classification algorithm examples

---
+++ examples/majority_classification.py
@@ -0,0 +1,17 @@
+from bhv.np import NumPyPacked64BHV as BHV
+
+N = 199*2
+
+xs = BHV.nrand(N)
+ys = [i % 2 == 0 for i in range(N)]
+
+pos = BHV.majority([x for x, y in zip(xs, ys) if y])
+neg = BHV.majority([x for x, y in zip(xs, ys) if not y])
+
+right = 0
+for x, y in zip(xs, ys):
+    spos = x.bit_error_rate(pos)
+    sneg = x.bit_error_rate(neg)
+    prediction = spos < sneg
+    right += prediction == y
+print(f"acc {right/N}")
