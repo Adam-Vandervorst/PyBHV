019c91da7fb8662c26d93ac07a62838cd54d3bca
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-04-24 04:28:37 +0200

    Clean up lsynthesis

--- tests/lsynthesis.py
+++ tests/lsynthesis.py
@@ -17,10 +17,11 @@ def synth(vs, t):
         return BHV.ONE if t[0] else BHV.ZERO
 
 
+names = [Var("a"), Var("b"), Var("c")]
 configurations = bitconfigs(3)
 operators = 0
 for target in bitconfigs(8):
-    f = synth([Var("a"), Var("b"), Var("c")], target)
+    f = synth(names, target)
     f_ = f.simplify()
 
     # print(f_.show())
@@ -33,7 +34,10 @@ for target in bitconfigs(8):
 
 print("average operators:", operators/256)
 
-print(tos(184, 8), synth([Var("a"), Var("b"), Var("c")], tomask(tos(184, 8))).simplify().show())
-print(tos(110, 8), synth([Var("a"), Var("b"), Var("c")], tomask(tos(110, 8))).simplify().show())
-print(tos(90, 8), synth([Var("a"), Var("b"), Var("c")], tomask(tos(90, 8))).simplify().show())
-print(tos(30, 8), synth([Var("a"), Var("b"), Var("c")], tomask(tos(30, 8))).simplify().show())
+print(tos(184, 8), synth(names, tomask(tos(184, 8))).simplify().show())
+print(tos(110, 8), synth(names, tomask(tos(110, 8))).simplify().show())
+print(tos(90, 8), synth(names, tomask(tos(90, 8))).simplify().show())
+print(tos(30, 8), synth(names, tomask(tos(30, 8))).simplify().show())
+
+maj3 = [sum(c) >= 2 for c in configurations]
+print(frommask(maj3), synth(names, maj3).simplify().show())
