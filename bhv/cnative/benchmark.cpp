07a25cf58ca246688e07527ce4e700e2f8a440d3
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-07-21 03:42:15 +0200

    Add bits_of_byte permutations

--- bhv/cnative/benchmark.cpp
+++ bhv/cnative/benchmark.cpp
@@ -176,34 +176,75 @@ float random_benchmark(bool display, bool keep_in_cache, float base_frac) {
 //#define RAND
 //#define RAND2
 //#define RANDOM
+#define PERMUTE
+
 #include "bitset"
 void print_bits(int64_t w) {
     std::bitset<64> x(w);
     std::cout << x << std::endl;
 }
 
+
+void print_byte(int8_t w) {
+    std::bitset<8> x(w);
+    std::cout << x << std::endl;
+}
+
+
 int main() {
+#ifdef PERMUTE
+    std::mt19937_64 rng;
+
+    uint64_t r = rng();
+
+    uint8_t r0 = 0b01110001;
+
+    uint64_t id = (uint64_t)_mm_set_pi8(7, 6, 5, 4, 3, 2, 1, 0);
+
+    uint64_t p1 = (uint64_t)_mm_set_pi8(7, 5, 3, 1, 6, 4, 2, 0);
+    uint64_t p1_inv = (uint64_t)_mm_set_pi8(7, 2, 6, 2, 5, 1, 4, 0);
+
+    uint64_t p2 = (uint64_t)_mm_set_pi8(7, 6, 5, 4, 0, 1, 2, 3);
+    uint64_t p2_inv = p2;
+
+    uint64_t rp1 = bhv::rand_bits_of_byte_permutation(2);
+    uint64_t rp1_inv = bhv::bits_of_byte_permutation_invert(rp1);
+
+
+    print_bits(rp1);
+    print_bits(rp1_inv);
+
+    print_byte(r0);
+
+    uint8_t res = bhv::permute_byte_bits(r0, rp1);
+    print_byte(res);
+
+    uint8_t res_ = bhv::permute_byte_bits(res, rp1_inv);
+    print_byte(res_);
+
+#endif
+
     // testing code
 
-        uint8_t to = 0;
-        float_t remaining = 0;
-    //    uint64_t instruction = bhv::instruction_upto(.0000001, &to, &remaining);  //
-    //    uint64_t instruction = bhv::instruction_upto(.5, &to, &remaining);  //
-    //    uint64_t instruction = bhv::instruction_upto(.625, &to); // 10
-    //    uint64_t instruction = bhv::instruction_upto(.375, &to);  // 01
-        uint64_t instruction = bhv::instruction_upto(.5625, &to, &remaining);  // 100
-//        uint64_t instruction = bhv::instruction_upto(123.f/256.f, &to, &remaining);  // 0111101
-//        uint64_t instruction = bhv::instruction_upto(.5625001, &to, &remaining);  // 100
-//        uint64_t instruction = bhv::instruction_upto(.5624999, &to, &remaining);  // 100
-
-        std::cout << "to: " << (uint32_t)to << std::endl;
-        std::cout << "rem: " << remaining << std::endl;
-        print_bits(instruction);
-
-        for (uint8_t i = 0; i < to; ++i)
-            std::cout << ((instruction & (1 << i)) >> i);
-
-        std::cout << std::endl;
+//        uint8_t to = 0;
+//        float_t remaining = 0;
+//    //    uint64_t instruction = bhv::instruction_upto(.0000001, &to, &remaining);  //
+//    //    uint64_t instruction = bhv::instruction_upto(.5, &to, &remaining);  //
+//    //    uint64_t instruction = bhv::instruction_upto(.625, &to); // 10
+//    //    uint64_t instruction = bhv::instruction_upto(.375, &to);  // 01
+//        uint64_t instruction = bhv::instruction_upto(.5625, &to, &remaining);  // 100
+////        uint64_t instruction = bhv::instruction_upto(123.f/256.f, &to, &remaining);  // 0111101
+////        uint64_t instruction = bhv::instruction_upto(.5625001, &to, &remaining);  // 100
+////        uint64_t instruction = bhv::instruction_upto(.5624999, &to, &remaining);  // 100
+//
+//        std::cout << "to: " << (uint32_t)to << std::endl;
+//        std::cout << "rem: " << remaining << std::endl;
+//        print_bits(instruction);
+//
+//        for (uint8_t i = 0; i < to; ++i)
+//            std::cout << ((instruction & (1 << i)) >> i);
+//
+//        std::cout << std::endl;
 
 
 
--- bhv/cnative/permutation.h
+++ bhv/cnative/permutation.h
@@ -1,3 +1,28 @@
+uint8_t permute_byte_bits(uint8_t x, uint64_t p) {
+    uint64_t w = _pdep_u64(x, 0x0101010101010101);
+    uint64_t res = (uint64_t)_mm_shuffle_pi8(_mm_cvtsi64_m64(w), _mm_cvtsi64_m64(p));
+    return _pext_u64(res, 0x0101010101010101);
+}
+
+uint64_t bits_of_byte_permutation_invert(uint64_t p) {
+    uint64_t r = 0;
+
+    for (uint64_t i = 0; i < 8; ++i)
+        r |= (i << ((p >> i*8) & 0x07)*8);
+
+    return r;
+}
+
+uint64_t rand_bits_of_byte_permutation(uint32_t seed) {
+    std::minstd_rand0 perm_rng(seed);
+
+    uint8_t p [8] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+    std::shuffle(p, p + 8, perm_rng);
+
+    return *((uint64_t*)p);
+}
+
 void permute_words_into(word_t * x, word_iter_t* word_permutation, word_t * target) {
     for (word_iter_t i = 0; i < WORDS; ++i) {
         target[i] = x[word_permutation[i]];
@@ -23,8 +48,22 @@ word_iter_t* rand_word_permutation(uint32_t seed) {
     return p;
 }
 
+void permute_bits_of_bytes_into(word_t * x, int32_t perm_id, word_t * target) {
+    if (perm_id == 0) memcpy(target, x, BYTES);
+    else {
+        uint8_t* x_bytes = (uint8_t*)x;
+        uint8_t* target_bytes = (uint8_t*)target;
+
+        uint64_t byte_perm = rand_bits_of_byte_permutation(perm_id);
+        if (perm_id < 0) byte_perm = bits_of_byte_permutation_invert(byte_perm);
+
+        for (byte_iter_t i = 0; i < BYTES; ++i)
+            target[i] = permute_byte_bits(x[i], byte_perm);
+    }
+}
+
 void permute_into(word_t * x, int32_t perm, word_t * target) {
-    if (perm == 0) *target = *x;
+    if (perm == 0) memcpy(target, x, BYTES);
     else if (perm > 0) permute_words_into(x, rand_word_permutation(perm), target);
     else inverse_permute_words_into(x, rand_word_permutation(-perm), target);
 }
