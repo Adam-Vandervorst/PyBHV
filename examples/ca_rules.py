a4eddc8098819372e446306d6279737e334fd980
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-09-07 14:45:45 +0200

    Fix rule numbering (assume little endian)

--- examples/ca_rules.py
+++ examples/ca_rules.py
@@ -1,18 +1,17 @@
 from bhv.symbolic import SymbolicBHV, Var
 from bhv.np import NumPyBoolBHV as BHV, DIMENSION
 from bhv.visualization import Image
-import numpy as np
 
 
 def make_rule(r: int):
-    mask = [b == '1' for b in bin(r)[2:].rjust(8, "0")]
+    mask = [b == '1' for b in reversed(bin(r)[2:].rjust(8, "0"))]
     formula = SymbolicBHV.synth([Var("left"), Var("center"), Var("right")], mask)
     formula = formula.simplify()
     print("formula:", formula.show())
     return lambda x: formula.execute(vars={"left": x.roll_bits(1), "center": x, "right": x.roll_bits(-1)})
 
 
-RULE = 90
+RULE = 30
 ITERATIONS = 10000
 
 rule = make_rule(RULE)
