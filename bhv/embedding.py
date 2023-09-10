8a8357c5e031034d51611cd33d2bd43e5afdfb7f
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-09-10 18:02:26 +0200

    Add intervals embedding

--- bhv/embedding.py
+++ bhv/embedding.py
@@ -1,4 +1,4 @@
-from random import random
+from random import random, uniform
 from bhv.abstract import AbstractBHV
 from typing import Generic, TypeVar, Type, Optional, Iterable
 
@@ -67,3 +67,62 @@ class Collapse(Embedding[Iterable[float]]):
         i = 1. - soft
         o = soft
         return (i if b else o for b in input_hv.bits())
+
+
+class Intervals:
+    @staticmethod
+    def _perfect_overlap(low: float, high: float, divisions: int, overlap: int, ends: bool):
+        """
+        Generate a list of intervals dividing a given range [low, high] into 'divisions' with a specified 'overlap'.
+
+        Example:
+        >>> Intervals._perfect_overlap(0.0, 1.0, divisions=5, overlap=2, ends=True)
+        [(0.0, 0.1), (0.0, 0.2), (0.1, 0.3), (0.2, 0.4), (0.3, 0.5), (0.4, 0.6),
+         (0.5, 0.7), (0.6, 0.8), (0.7, 0.9), (0.8, 1.0), (0.9, 1.0)]
+
+        >>> Intervals._perfect_overlap(0.0, 1.0, divisions=5, overlap=2, ends=False)
+        [(0.0, 0.2), (0.1, 0.3), (0.2, 0.4), (0.3, 0.5), (0.4, 0.6),
+         (0.5, 0.7), (0.6, 0.8), (0.7, 0.9), (0.8, 1.0)]
+        """
+        step = (high - low) / divisions
+        offset = step / overlap
+        intervals = []
+
+        if ends:
+            intervals.append((low, low + offset))
+
+        current_low = low
+        while current_low + step <= high:
+            current_high = current_low + step
+            intervals.append((current_low, current_high))
+            current_low += offset
+
+        if ends and current_low < high:
+            intervals.append((current_low, high))
+
+        return intervals
+
+    @classmethod
+    def perfect(cls, hvt, low: float = 0., high: float = 1., divisions: int = 5, overlap: int = 2, ends: bool = False):
+        return cls(hvt, cls._perfect_overlap(low, high, divisions, overlap, ends))
+
+    def __init__(self, hvt, intervals):
+        self.hvt = hvt
+        self.intervals = intervals
+        ls, hs = zip(*intervals)
+        self.span = (min(ls), max(hs))
+        self.hvs = [hvt.rand() for _ in intervals]
+
+    def forward(self, x: float):
+        matching = [hv for hv, (l, h) in zip(self.hvs, self.intervals) if l <= x <= h]
+        return self.hvt.majority(matching)
+
+    def back(self, input_hv, threshold=4):
+        L, H = self.span
+        for hv, (l, h) in zip(self.hvs, self.intervals):
+            if not hv.unrelated(input_hv, threshold):
+                L = max(L, l)
+                H = min(H, h)
+                if L > H:
+                    return
+        return uniform(L, H)
