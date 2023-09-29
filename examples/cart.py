51c985ac64e90cae153bc1a05a481e9fc39f40d5
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-09-29 21:24:12 +0200

    Extra cart attempts

--- examples/cart.py
+++ examples/cart.py
@@ -18,7 +18,27 @@ def embed2(x, y):
     yp = BHV.select_random(C, Y, y)
     return BHV.select_rand(xp, yp)
 
-grid = [[embed(x/S, y/S)
+def embed3(x, y):
+    xp = (~X).select_random(X, x/2 + .25)
+    yp = (~Y).select_random(Y, y/2 + .25)
+    return C.select(xp, yp)
+
+
+def embed4(x, y):
+    br = (~Y).select_rand(X)
+    bl = (~Y).select_rand(~X)
+    tr = (Y).select_rand(X)
+    tl = (Y).select_rand(~X)
+
+    th = BHV.select_random(tl, tr, x)
+    bh = BHV.select_random(bl, br, x)
+    lv = BHV.select_random(tl, bl, y)
+    rv = BHV.select_random(tr, br, y)
+
+    return C.select(BHV.select_random(th, bh, y),
+                    BHV.select_random(lv, rv, x))
+
+grid = [[embed4(x/S, y/S)
         for y in range(S+1)]
         for x in range(S+1)]
 
@@ -47,8 +67,9 @@ for x1, y1 in ps:
         euclid = hypot((x2 - x1)/S, (y2 - y1)/S)
         manhat = (abs(x2 - x1) + abs(y2 - y1))/S
 
-        # hvd = grid[x1][y1].cosine(grid[x2][y2])
-        hvd = 2*grid[x1][y1].jaccard(grid[x2][y2], distance=True)
+        # hvd = grid[x1][y1].cosine(grid[x2][y2], distance=True)
+        # hvd = 2*grid[x1][y1].jaccard(grid[x2][y2], distance=True)
+        hvd = grid[x1][y1].bit_error_rate(grid[x2][y2])
 
         print((x1, y1), (x2, y2))
         print(round(euclid, 3), round(manhat, 3), round(hvd, 3))
